6e8f0ef2b0b7e493e85c461bfabee4b6371cb969
Eben Haezer Saputra <61021650+sigkarmoil@users.noreply.github.com>
2026-04-21 11:38:56 -0400

    Changed the gdscript to solely use animation player
    dropped of the requirement to use AnimationTree

--- characters/player_idle_state.gd
+++ characters/player_idle_state.gd
@@ -1,16 +1,16 @@
-extends State
+extends CharacterBody2D
 
-class_name IdleState
+# Direct reference to the AnimationPlayer node named "anim" on this Player.
+# This is the only way animations are played in this script.
+@onready var anim: AnimationPlayer = $anim
 
-func enter():
-	print("Player at idle state")
-	
-func handle_input(event: InputEvent):
-	if Input.is_action_pressed("parry"):
-		state_machine.change_state("player_counter_parry")
-	elif Input.is_action_pressed("dodge"):
-		state_machine.change_state("player_counter_dodge")
-	elif Input.is_action_pressed("use_relic"):
-		state_machine.change_state("player_use_relic")
-		
-	
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("parry"):
+		anim.play("player_counter_parry")
+	elif Input.is_action_just_pressed("dodge"):
+		anim.play("player_counter_dodge")
+	elif Input.is_action_just_pressed("use_relic"):
+		anim.play("player_use_relic")
+	elif not anim.is_playing():
+		anim.play("player_idle")
