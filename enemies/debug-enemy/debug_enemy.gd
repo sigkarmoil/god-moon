460e42a1b43aaaa89a37bc0b21dba6b4cb161f84
Eben Haezer Saputra <61021650+sigkarmoil@users.noreply.github.com>
2026-04-22 14:11:55 -0400

    minor enemy animation update

--- enemies/debug-enemy/debug_enemy.gd
+++ enemies/debug-enemy/debug_enemy.gd
@@ -1,13 +1,22 @@
 extends CharacterBody2D
 
-var attack_variant: int
+var animations: Array = [
+	"multi_attack",
+    "strong_attack"
+]
 
-func _ready() -> void:
-	$AnimationPlayer.play("idle")
-	
-func _physics_process(delta: float) -> void:
-	pass
+@export var pause_duration: float = 1.0
 
+func _on_Timer_timeout() -> void:
+	await play_sequence(3)
+	await get_tree().create_timer(pause_duration).timeout
+	$Timer.start()
 
-func _on_timer_timeout() -> void:
-	$AnimationPlayer.play("multi_attack")
+func play_sequence(count: int):
+	var pool = animations.duplicate()
+	pool.shuffle()
+	var selected = pool.slice(0, count)
+
+	for anim in selected:
+		$AnimationPlayer.play(anim)
+		await $AnimationPlayer.animation_finished
